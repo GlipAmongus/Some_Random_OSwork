6622a80a804dc5ed5fadf9971691ee6069664aa4
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-04-25 09:24:19 +0200

    bitmap struct, allocate_emap

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+
+#define MIN_TID 300
+#define MAX_TID 5000
+
+//Task 1 Question A
+typedef struct bitmap{
+    uint8_t *bits;
+    size_t size_in_bytes;
+    size_t occupied;
+} bitmap_t;
+
+bitmap_t *map;
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    /*
+     *      Data structures options:
+     *  Bitmap, implement queue to avoid bigO(N) complexity on freed locations
+     *
+     * */
+
+
+}
+
+//
+int allocate_map (void)
+{
+    map = malloc(sizeof(bitmap_t));
+    if(map == NULL)
+        return -1;
+
+    map->occupied = 0;
+    map->size_in_bytes = MAX_TID-MIN_TID;
+
+    map->bits = calloc(map->size_in_bytes, sizeof(uint8_t));
+    if(map->bits == NULL)
+        return -1;
+
+    return 1;
 }
