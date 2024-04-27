1606da4905dbd2278468f245ed223670acb87821
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-04-27 16:46:38 +0200

    fixed: memset, bit indexing, last uint32

--- main.c
+++ main.c
@@ -44,43 +44,11 @@ bitmap_t *map;
 
 int allocate_map (void);
 int allocate_tid (void);
-void release_tid(int tid);
+void release_tid (int tid);
 
 
-int main() {
-    /*
-     *      Data structures options:
-     *  Stack, initialized with all TIDs, pop when being unavailable, push when available
-     * */
-    if(allocate_map() == 1)
-        printf("allocate_map succeed\n");
+int main() {}
 
-    if(allocate_tid() == 1)
-    {
-        printf("id found available\n");
-        printf("%u\n", map->array[0]);
-    }else{
-        printf("id found unavailable\n");
-    }
-    if(allocate_tid() == 1)
-    {
-        printf("id found available\n");
-        printf("%u\n", map->array[0]);
-    }else{
-        printf("id found unavailable\n");
-    }
-    if(allocate_tid() == 1)
-    {
-        printf("id found available\n");
-        printf("%u\n", map->array[0]);
-    }else{
-        printf("id found unavailable\n");
-    }
-    release_tid(300);
-    printf("%u\n", map->array[0]);
-}
-
-//
 int allocate_map (void)
 {
     map = malloc(sizeof(bitmap_t));
@@ -90,21 +58,23 @@ int allocate_map (void)
     map->occupied = 0;
     map->size_in_bytes = ((MAX_TID-MIN_TID)/32) + 1; //4700/32 = 146 + 1 = 147
 
-    map->array = calloc(map->size_in_bytes, sizeof(uint8_t));
+    map->array = calloc(map->size_in_bytes, sizeof(uint32_t));
     if(map->array == NULL)
         return -1;
 
-    memset(map->array, 0, map->size_in_bytes);
+    for(int i = 0; i < map->size_in_bytes; i++)
+        map->array[i] = 0;
+
     return 1;
 }
 
 int allocate_tid(void)
 {
-    for(int i = 0; i < map->size_in_bytes - 1; i++)
+    for(int i = 0; i < map->size_in_bytes-1; ++i){ //A[0] - A[145]
     {
         if(map->array[i] != 0xFFFFFFFF)
         {
-            for(int j = 0; j < 32; j++)
+            for(int j = i*32; j < (i+1) * 32; ++j)
             {
                 if(!TestBit(map->array, j))
                 {
@@ -114,11 +84,12 @@ int allocate_tid(void)
             }
         }
     }
-    if(map->array[147] != 0xFFFFFFF0)
+    if(map->array[146] != 0x0FFFFFFF)   //A[146]
     {
-        for(int j = 0; j < 28; ++j)
+        for(int j = 146*32; j < 147 * 32; j++)
         {
-            if(TestBit(map->array, j)) {
+            if(!TestBit(map->array, j))
+            {
                 SetBit(map->array, j);
                 return 1;
             }
